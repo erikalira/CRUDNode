593e98ad3ffcfa89565bd2846ae1b3c77d9322b9
Erika Lira <liraerika00@gmail.com>
2021-05-07 23:01:53 -0300

    fix: conexao de db de ambientes

--- config/database.js
+++ config/database.js
@@ -1,15 +1,34 @@
 module.exports = {
-  "username": process.env.USERNAME || "",
-  "password": process.env.PASSSWORD || "",
-  "database": process.env.DATABASE || "products",
-  "dialect": process.env.DIALECT || "sqlite",
-  "storage": process.env.STORAGE || "database.sqlite",
-  "host": process.env.HOST || "",
-  "port": process.env.PORT || "",
-  "dialectOptions": {
-    "ssl": {
-      "require": true,
-      "rejectUnauthorized": false
+  development: {
+    "username": "",
+    "password": "",
+    "database": "products",
+    "dialect": "sqlite",
+    "storage": "database.sqlite"
+  },
+  test: {
+    username: process.env.CI_DB_USERNAME,
+    password: process.env.CI_DB_PASSWORD,
+    database: process.env.CI_DB_NAME,
+    host: '127.0.0.1',
+    port: 3306,
+    dialect: 'mysql',
+    dialectOptions: {
+      bigNumberStrings: true
+    }
+  },
+  production: {
+    "username": process.env.USERNAME,
+    "password": process.env.PASSSWORD,
+    "database": process.env.DATABASE,
+    "dialect": "postgres",
+    "host": process.env.HOST,
+    "port": process.env.PORT,
+    "dialectOptions": {
+      "ssl": {
+        "require": true,
+        "rejectUnauthorized": false
+      }
     }
   }
 }
